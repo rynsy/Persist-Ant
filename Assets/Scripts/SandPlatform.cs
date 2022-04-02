5212b97a05f8ca6611a21ff05b69dd549300a6a1
Ryan Lindsey <ryan.lindsey@uky.edu>
2022-04-02 04:56:13 -0400

    Added the sand platforms. The parameters definitely need tweaking

--- Assets/Scripts/SandPlatform.cs
+++ Assets/Scripts/SandPlatform.cs
@@ -6,20 +6,36 @@ public class SandPlatform : MonoBehaviour
     public AudioClip dissolveSound1; 
     public AudioClip dissolveSound2;
 
+    public int currentSprite = 0;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] dissolveSprites;
+
     public float dissolveTime;    
+    public float dissolveDelay;    
 
     void Awake ()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Call Coroutine to dissolve platform
+        if (collision.gameObject.tag == "Player")
+        {
+            SoundManager.instance.RandomizeSfx (dissolveSound1, dissolveSound2); 
+            InvokeRepeating("Dissolve", 0f, 0.5f);
+        }
     }
 
     public void Dissolve()
     {
-        // We want this platform to dissolve in around the time it takes to play the clip/animation
-        SoundManager.instance.RandomizeSfx (dissolveSound1, dissolveSound2); 
+        if ( currentSprite > dissolveSprites.Length - 1 )
+        {
+            Destroy(gameObject);
+            return;
+        }
+        spriteRenderer.sprite = dissolveSprites[currentSprite];
+        currentSprite += 1;
     }
 }
