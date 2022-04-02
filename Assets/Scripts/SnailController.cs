3a8e1eca5fa4b57b3257e7afe77a04b002180f97
Ryan Lindsey <ryan.lindsey@uky.edu>
2022-04-02 16:19:04 -0400

    Added controller for Snail

--- Assets/Scripts/SnailController.cs
+++ Assets/Scripts/SnailController.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LadybugController : MonoBehaviour
+public class SnailController : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
 
-    public float bugSpeed = 1f;
+    public float bugSpeed = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +21,6 @@ public class LadybugController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rigidbody.velocity = new Vector2(transform.right.x * bugSpeed , rigidbody.velocity.y);
+        rigidbody.velocity = new Vector2(-transform.right.x * bugSpeed , rigidbody.velocity.y);
     }
 }
