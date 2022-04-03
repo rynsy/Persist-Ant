ae84fbe1fb4527635c23988818565c6e13ed80e7
Dustin Davis <bloobakon@gmail.com>
2022-04-02 22:44:42 -0500

    Snail and fly now die.

--- Assets/Scripts/BugScripts/FlyController.cs
+++ Assets/Scripts/BugScripts/FlyController.cs
@@ -6,6 +6,7 @@ public class FlyController : MonoBehaviour
 {
     private float counter;
     private const float damp = 0.001f;
+    private Animator animator;
 
 
     public float flySpeed = 0.5f;
@@ -14,13 +15,17 @@ public class FlyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         counter = Random.Range(0, 2 * Mathf.PI);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("FlyDead");
+        }
     }
 
     private void FixedUpdate()
@@ -28,5 +33,10 @@ public class FlyController : MonoBehaviour
         transform.position = new Vector2( transform.position.x + (damp * -1 * flySpeed) , transform.position.y + (bobFactor * Mathf.Cos(counter)));
         counter += 0.1f;
         counter = counter % (2 * Mathf.PI);
+    }
+    
+    public void DestroyFly()
+    {
+        Destroy(gameObject);
     }
 }
