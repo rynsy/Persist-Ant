cf9bfebe43ed22162ff74bc8eb580e20d00d3bd6
Dustin Davis <dustin.davis@guardcite.org>
2022-04-14 10:57:16 -0500

    fade transition

--- Assets/Scripts/MenuScripts/BootysmackController.cs
+++ Assets/Scripts/MenuScripts/BootysmackController.cs
@@ -1,21 +1,92 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+//public class bootysmackcontroller : monobehaviour
+//{
+//    // start is called before the first frame update
+//    void start()
+//    {
+//        //start the coroutine we define below named examplecoroutine.
+//        startcoroutine(gotostartmenu());
+//    }
+
+//    ienumerator gotostartmenu()
+//    {
+//        soundmanager.instance.playbooty();
+//        yield return new waitforseconds(4);
+//        scenemanager.loadscene(scenename: "startmenu");
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BootysmackController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Start the coroutine we define below named ExampleCoroutine.
-        StartCoroutine(GoToStartMenu());
-    }
+	public GameObject soundManager;
 
-    IEnumerator GoToStartMenu()
-    {
-        SoundManager.instance.PlayBooty();
-        yield return new WaitForSeconds(4);
-        SceneManager.LoadScene(sceneName: "StartMenu");
-    }
+	#region FIELDS
+	public Image fadeOutUIImage;
+	public float fadeSpeed = 0.8f;
+
+	public enum FadeDirection
+	{
+		In, //Alpha = 1
+		Out // Alpha = 0
+	}
+	#endregion
+
+	#region MONOBHEAVIOR
+	void OnEnable()
+	{
+		StartCoroutine(Fade(FadeDirection.Out));
+	}
+	#endregion
+
+	#region FADE
+	private IEnumerator Fade(FadeDirection fadeDirection)
+	{
+		SoundManager.instance.PlayBooty();
+		float alpha = (fadeDirection == FadeDirection.Out) ? 1 : 0;
+		float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 0 : 1;
+		if (fadeDirection == FadeDirection.Out)
+		{
+			while (alpha >= fadeEndValue)
+			{
+				SetColorImage(ref alpha, fadeDirection);
+				yield return null;
+			}
+			fadeOutUIImage.enabled = false;
+		}
+		else
+		{
+			fadeOutUIImage.enabled = true;
+			while (alpha <= fadeEndValue)
+			{
+				SetColorImage(ref alpha, fadeDirection);
+				yield return null;
+			}
+		}
+		SceneManager.LoadScene(sceneName: "StartMenu");
+	}
+	#endregion
+
+	#region HELPERS
+	public IEnumerator FadeAndLoadScene(FadeDirection fadeDirection, string sceneToLoad)
+	{
+		yield return Fade(fadeDirection);
+		SceneManager.LoadScene(sceneToLoad);
+	}
+
+	private void SetColorImage(ref float alpha, FadeDirection fadeDirection)
+	{
+		fadeOutUIImage.color = new Color(fadeOutUIImage.color.r, fadeOutUIImage.color.g, fadeOutUIImage.color.b, alpha);
+		alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.Out) ? -1 : 1);
+	}
+	#endregion
 }
